30142a4b4c8691682e2ca184d895a0ba4232e41a
Chelsea Ferguson <c.fergie96@icloud.com>
2024-11-15 02:01:44 +1300

    changed port

--- backend/server.js
+++ backend/server.js
@@ -4,7 +4,7 @@ const mongoose = require('mongoose');
 const cors = require('cors');
 
 const app = express();
-const PORT = 5000;
+const PORT = process.env.PORT;
 
 app.use(cors());
 app.use(express.json());
