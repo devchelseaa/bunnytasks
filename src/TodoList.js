bbf3fcb54ab1dded9aa921175eabbb3d5f820d9c
Chelsea Ferguson <c.fergie96@icloud.com>
2024-11-15 01:44:14 +1300

    deployment troubleshooting

--- src/TodoList.js
+++ src/TodoList.js
@@ -5,7 +5,7 @@ const TodoList = () => {
   const [taskName, setTaskName] = useState('');
 
   // Use API URL defined in .env file
-  const apiUrl = 'https://bunnytasks-0e684a5d384c.herokuapp.com/bunnytasks/tasks';
+  const apiUrl = 'https://bunnytasks-0e684a5d384c.herokuapp.com/bunnytasks';
 
   useEffect(() => {
     // Fetch tasks from the backend
